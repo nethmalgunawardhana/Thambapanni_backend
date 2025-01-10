e096bd4c142e8c6c74fcfe857348657a924d68ed
nethmalgunawardhana <nethmalgunawardhana@gmail.com>
2025-01-10 19:51:01 +0530

    Update destination route to accept type as a URL parameter

--- src/routes/destinationRoutes.js
+++ src/routes/destinationRoutes.js
@@ -2,5 +2,5 @@ const express = require('express');
 const router = express.Router();
 const { getDestinationsByType } = require('../controllers/destinationController');
 
-router.get('/type', getDestinationsByType);
+router.get('/:type', getDestinationsByType);
 module.exports = router;
