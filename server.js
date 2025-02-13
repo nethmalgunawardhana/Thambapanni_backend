9369722724d5e9b8c4410c90affed27107a68b49
nethmalgunawardhana <nethmalgunawardhana@gmail.com>
2025-02-13 17:50:52 +0530

    Add Stripe integration for payment processing and create payment intent endpoint

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const destinationRoutes = require('./src/routes/destinationRoutes');
 const guideRoutes = require('./src/routes/guideRoutes');
 const tripRoutes = require('./src/routes/tripRoutes');
 const imageRoutes = require('./src/routes/imageRoutes');
+const paymentRoutes = require('./src/routes/paymentRoutes');
 const app = express();
 
 
@@ -17,6 +18,7 @@ const app = express();
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
+
 
 // Routes
 app.use('/auth', authRoutes); // All auth routes will be prefixed with /auth
@@ -26,6 +28,7 @@ app.use('/destinations',destinationRoutes);
 app.use('/guides', guideRoutes);
 app.use('/api', tripRoutes);
 app.use('/images', imageRoutes);
+app.use('/api/payments', paymentRoutes);
 // Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
---
+++ src/routes/paymentRoutes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const { createPaymentIntent } = require('../controllers/paymentController');
+
+const router = express.Router();
+
+router.post('/create-payment-intent', createPaymentIntent);
+
+
+module.exports = router;
