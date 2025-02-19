a797463809f2a0b95a473c03d693aa394d12fd48
AdithaBuwaneka <adithabuwaneka0@gmail.com>
2025-02-19 16:38:28 +0530

    Increase rate limit to 1000 requests per 15 minutes

--- src/routes/tripRoutes.js
+++ src/routes/tripRoutes.js
@@ -7,7 +7,7 @@ const router = express.Router();
 // set up rate limiter: maximum of 100 requests per 15 minutes
 const limiter = RateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // max 100 requests per windowMs
+  max: 1000, // max 100 requests per windowMs
 });
 
 router.post('/generate-trip-plan', generateTripPlan);
