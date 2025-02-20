944b1025a715def30f5aaa1c509b1962e0017574
nethmalgunawardhana <nethmalgunawardhana@gmail.com>
2025-02-20 14:30:26 +0530

    Rename route for retrieving bookmarked trips to improve clarity

--- src/routes/bookmarkroutes.js
+++ src/routes/bookmarkroutes.js
@@ -7,6 +7,6 @@ const bookmarkController = require('../controllers/bookmarkcontroller');
 router.get('/ids', verifyToken, bookmarkController.getBookmarkedIds);
 router.post('/add', verifyToken, bookmarkController.addBookmark);
 router.post('/remove', verifyToken, bookmarkController.removeBookmark);
-router.get('/', verifyToken, bookmarkController.getBookmarkedTrips);
+router.get('/get', verifyToken, bookmarkController.getBookmarkedTrips);
 
 module.exports = router;
