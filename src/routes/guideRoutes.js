889cf2c285571113c1bececeb471ade68a50c535
AdithaBuwaneka <adithabuwaneka0@gmail.com>
2025-02-22 18:36:14 +0530

    Implement rate limiting for guide request routes to enhance API security

--- src/routes/guideRoutes.js
+++ src/routes/guideRoutes.js
@@ -4,17 +4,22 @@ const { submitGuideApplication , adminVerifyApplication , getVerifiedGuides,conf
 const { getApplicationStatus } = require('../controllers/guideController');
 const upload = require('../middleware/upload');
 const { verifyToken } = require('../middleware/authMiddleware');
-
+const RateLimit = require('express-rate-limit');
 const router = express.Router();
 
+// set up rate limiter: maximum of 100 requests per 15 minutes
+const limiter = RateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // max 100 requests per windowMs
+});
 // POST /api/guides/apply
 router.post('/apply', verifyToken,submitGuideApplication);
 router.get('/admin-verify',adminVerifyApplication);
 router.get('/status',verifyToken,getApplicationStatus)
 // GET /api/guides/verified
 router.get('/verified', getVerifiedGuides);
-router.post('/request-confirmation', verifyToken, confirmGuideRequest);
-router.get('/guide-response', handleGuideResponse);
-router.get('/confirmation-status/:tripId', verifyToken, getGuideConfirmationStatus);
+router.post('/request-confirmation', verifyToken,  limiter,confirmGuideRequest);
+router.get('/guide-response',  limiter,handleGuideResponse);
+router.get('/confirmation-status/:tripId', limiter,verifyToken, getGuideConfirmationStatus);
 
 module.exports = router;
